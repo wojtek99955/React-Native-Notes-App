d51b64fd99a0a6b1c653efc0cc3968c9cb51456b
Wojtek Książek <wojtek99955@gmail.com>
2022-05-25 15:38:11 +0200

    note added

---
+++ components/Note.js
@@ -0,0 +1,23 @@
+import { View, Text } from "react-native";
+import styled from "styled-components";
+const Container = styled.View`
+  background-color: white;
+  margin: 10px auto;
+  width: 70%;
+  padding: 10px;
+  border-radius: 10px;
+`;
+
+const NoteText = styled.Text`
+  font-size: 25px;
+`;
+
+const Note = ({ item }) => {
+  return (
+    <Container>
+      <NoteText>{item.text}</NoteText>
+    </Container>
+  );
+};
+
+export default Note;
