f14a18595aab3e8b0ef82e42fd27f67d613caec2
Wojtek Książek <wojtek99955@gmail.com>
2022-05-25 11:41:18 +0200

    add note added

---
+++ components/AddNote.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { View, Text } from "react-native";
+
+function AddNote() {
+  return (
+    <View>
+      <Text>Add Note</Text>
+    </View>
+  );
+}
+
+export default AddNote;
