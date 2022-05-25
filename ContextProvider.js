8043be39a2ef1ccf3da885920254f29772b6fd5c
Wojtek Książek <wojtek99955@gmail.com>
2022-05-25 14:52:38 +0200

    bug fixed

--- ContextProvider.js
+++ ContextProvider.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { createContext, useState } from "react";
 
-export const Context = createContext([]);
+export const Context = createContext();
 
 function ContextProvider({ children }) {
-  const [notes, setNotes] = useState();
+  const [notes, setNotes] = useState([]);
   return (
     <Context.Provider value={{ notes, setNotes }}>{children}</Context.Provider>
   );
