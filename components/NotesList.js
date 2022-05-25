f22b53e8fb1a80e1476905ee6ca204ff67217f1c
Wojtek Książek <wojtek99955@gmail.com>
2022-05-25 17:11:55 +0200

    add icon

--- components/NotesList.js
+++ components/NotesList.js
@@ -1,10 +1,16 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { useContext } from "react";
 import { Context } from "../ContextProvider";
 import Note from "./Note";
 import SearchBar from "./SearchBar";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import styled from "styled-components";
 
-const NoteList = () => {
+const IconContainer = styled.TouchableOpacity`
+  align-items: center;
+`;
+
+const NoteList = ({ navigation }) => {
   const ctx = useContext(Context);
   const { notes } = ctx;
 
@@ -13,6 +19,11 @@ const NoteList = () => {
     <View>
       <SearchBar />
       <FlatList data={ctx.notes} renderItem={Note} />
+      {notes.length > 2 ? (
+        <IconContainer onPress={() => navigation.navigate("Add Note")}>
+          <Ionicons name="add-circle" color="#fdbe00" size={60} />
+        </IconContainer>
+      ) : null}
     </View>
   );
 };
