ff60097864596dcdd83a6a619bd7e7aae8c556a2
Wojtek Książek <wojtek99955@gmail.com>
2022-05-25 11:47:06 +0200

    tabs added

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import ContextProvider from "./ContextProvider";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import AddNote from "./components/AddNote";
+import NotesList from "./components/NotesList";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function App() {
+  const Tabs = createBottomTabNavigator();
   return (
     <ContextProvider>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <Tabs.Navigator>
+          <Tabs.Screen name="Add Note" component={AddNote} />
+          <Tabs.Screen name="Notes List" component={NotesList} />
+        </Tabs.Navigator>
+      </NavigationContainer>
     </ContextProvider>
   );
 }
