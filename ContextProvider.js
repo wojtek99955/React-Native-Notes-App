2e2e8ae866f04d8dc8a4833809fb6a94abe37ff5
Wojtek Książek <wojtek99955@gmail.com>
2022-05-25 11:30:26 +0200

    context provider added

---
+++ ContextProvider.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { createContext, useState } from "react";
+
+export const context = createContext([]);
+
+function ContextProvider({ children }) {
+  const [notes, setNotes] = useState();
+  return (
+    <Context.Provider value={{ notes, setNotes }}>{children}</Context.Provider>
+  );
+}
+
+export default ContextProvider;
