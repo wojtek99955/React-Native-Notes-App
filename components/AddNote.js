e97e932827474a8da780138e1244241577373f7d
Wojtek Książek <wojtek99955@gmail.com>
2022-05-25 12:32:59 +0200

    input vertical align fixed

--- components/AddNote.js
+++ components/AddNote.js
@@ -3,12 +3,11 @@ import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
 const Container = styled.View``;
 const Input = styled.TextInput`
-  height: 150px;
   width: 100%;
   background-color: white;
   padding: 15px;
-  margin: auto;
   font-size: 25px;
+  height: 200px;
 `;
 const Title = styled.Text`
   font-size: 30px;
@@ -36,7 +35,12 @@ function AddNote() {
   return (
     <Container>
       <Title>Create Your Note!</Title>
-      <Input placeholder="add note" multiline={true} numberOfLines={8} />
+      <Input
+        placeholder="add note"
+        multiline={true}
+        numberOfLines={8}
+        textAlignVertical="top"
+      />
       <Button>
         <ButtonText>Add Note</ButtonText>
       </Button>
