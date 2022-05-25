81723f8ae73531afee7bfb9a1134c8a50c26f3b7
Wojtek Książek <wojtek99955@gmail.com>
2022-05-25 11:42:12 +0200

    NotesList added

---
+++ components/NotesList.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { View, Text } from "react-native";
+
+import React from "react";
+
+function NotesList() {
+  return (
+    <View>
+      <Text>Notes List</Text>
+    </View>
+  );
+}
+
+export default NotesList;
