e8a965cd1b3f52bb53a7b8090229d4c018578419
Wojtek Książek <wojtek99955@gmail.com>
2022-05-25 16:25:47 +0200

    search added

---
+++ components/SearchBar.js
@@ -0,0 +1,15 @@
+import { View, TextInput } from "react-native";
+import styled from "styled-components";
+
+const Container = styled.View``;
+const Input = styled.TextInput``;
+
+const SearchBar = () => {
+  return (
+    <Container>
+      <Input />
+    </Container>
+  );
+};
+
+export default SearchBar;
