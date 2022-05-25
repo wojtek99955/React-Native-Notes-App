5c32002799572b2a3f64f4a278cb41e198707050
Wojtek Książek <wojtek99955@gmail.com>
2022-05-25 16:34:56 +0200

    styles update

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -2,12 +2,19 @@ import { View, TextInput } from "react-native";
 import styled from "styled-components";
 
 const Container = styled.View``;
-const Input = styled.TextInput``;
+const Input = styled.TextInput`
+  background-color: #cecece;
+  height: 50px;
+  border-radius: 5px;
+  width: 80%;
+  margin: 20px auto;
+  padding: 10px;
+`;
 
 const SearchBar = () => {
   return (
     <Container>
-      <Input />
+      <Input placeholder="Search notes" />
     </Container>
   );
 };
