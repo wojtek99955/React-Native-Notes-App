942b6b2850031e089eb40f6be3559cdcdf287062
Wojtek Książek <wojtek99955@gmail.com>
2022-05-25 12:21:05 +0200

    content update

--- components/AddNote.js
+++ components/AddNote.js
@@ -1,11 +1,46 @@
 import styled from "styled-components";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+
+const Container = styled.View``;
+const Input = styled.TextInput`
+  height: 150px;
+  width: 100%;
+  background-color: white;
+  padding: 15px;
+  margin: auto;
+  font-size: 25px;
+`;
+const Title = styled.Text`
+  font-size: 30px;
+  text-align: center;
+  margin: 20px 0;
+`;
+
+const Button = styled.TouchableOpacity`
+  background-color: #fdbe00;
+  width: 150px;
+  height: 60px;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  margin-top: 50px;
+`;
+const ButtonText = styled.Text`
+  color: white;
+  font-size: 20px;
+  font-weight: 600;
+`;
 
 function AddNote() {
   return (
-    <View>
-      <Text>Add Note</Text>
-    </View>
+    <Container>
+      <Title>Create Your Note!</Title>
+      <Input placeholder="add note" multiline={true} numberOfLines={8} />
+      <Button>
+        <ButtonText>Add Note</ButtonText>
+      </Button>
+    </Container>
   );
 }
 
