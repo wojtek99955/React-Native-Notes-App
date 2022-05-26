204aa96ac86a66ecc3ed3fb360a2736f036744bd
Wojtek Książek <wojtek99955@gmail.com>
2022-05-26 13:19:25 +0200

    styles update

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,13 +1,18 @@
 import { View, TextInput } from "react-native";
 import styled from "styled-components";
 import Ionicons from "@expo/vector-icons/Ionicons";
-const Wrapper = styled.View``;
+const Wrapper = styled.View`
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  padding: 0 5px;
+`;
 const Container = styled.View`
   justify-items: center;
   align-items: center;
-  width: 80%;
+  width: 100%;
   background-color: #cecece;
-  margin: 20px auto;
+  margin: 20px 10px;
   padding: 10px;
   flex-direction: row;
   border-radius: 10px;
@@ -20,10 +25,12 @@ const Input = styled.TextInput`
 
 const SearchBar = () => {
   return (
-    <Container>
-      <Ionicons name="search-outline" size={20} />
-      <Input placeholder="Search notes" />
-    </Container>
+    <Wrapper>
+      <Container>
+        <Ionicons name="search-outline" size={20} />
+        <Input placeholder="Search notes" />
+      </Container>
+    </Wrapper>
   );
 };
 
