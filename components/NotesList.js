1e7565f3c35e7486109b3543235becad9c1062ea
Wojtek Książek <wojtek99955@gmail.com>
2022-05-27 17:51:35 +0200

    details added

--- components/NotesList.js
+++ components/NotesList.js
@@ -1,10 +1,33 @@
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Dimensions,
+  Button,
+} from "react-native";
 import { useContext } from "react";
 import { Context } from "../ContextProvider";
 import Note from "./Note/Note";
 import SearchBar from "./SearchBar/SearchBar";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import styled from "styled-components";
+
+export const Container = styled.View`
+  background-color: white;
+  margin: 5px;
+  padding: 22px 10px;
+  border-radius: 10px;
+  box-shadow: 2px 1px 6px rgba(206, 206, 206, 0.25);
+`;
+export const DateText = styled.Text`
+  font-size: 12px;
+  margin-top: 20px;
+`;
+
+export const NoteText = styled.Text`
+  font-size: 25px;
+`;
 
 const IconContainer = styled.TouchableOpacity`
   align-items: center;
@@ -24,6 +47,20 @@ const NoteList = ({ navigation }) => {
     } else {
       return null;
     }
+  };
+
+  const Note = ({ item }) => {
+    const NoteWidth = Dimensions.get("window").width;
+    return (
+      <Container style={{ width: (NoteWidth - 20) / 2 }}>
+        <NoteText>{item.text}</NoteText>
+        <DateText>{item.date}</DateText>
+        <Button
+          title="details"
+          onPress={() => navigation.navigate("Details", item)}
+        />
+      </Container>
+    );
   };
 
   console.log(notes);
