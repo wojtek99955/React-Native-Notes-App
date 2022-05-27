7293d3151a2f788467b84b71b9692b48430fd6c6
Wojtek Książek <wojtek99955@gmail.com>
2022-05-28 00:05:00 +0200

    settings screens added

---
+++ screens/SettingsScreens.js
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Settings from "../components/Settings";
+import Preferences from "../Preferences";
+
+const SettingsScreens = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Settings" component={Settings} />
+      <Stack.Screen name="Preferences" component={Preferences} />
+    </Stack.Navigator>
+  );
+};
+
+export default SettingsScreens;
