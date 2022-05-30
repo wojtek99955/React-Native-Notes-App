ac21b19ebb6b252d0ee8501d2fedb0b56658cc70
Wojtek Książek <wojtek99955@gmail.com>
2022-05-30 16:47:13 +0200

    styles update

--- components/Settings/FontSize.js
+++ components/Settings/FontSize.js
@@ -1,21 +1,47 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { useContext } from "react";
 import { Context } from "../../ContextProvider";
+import styled from "styled-components";
 
+const StyledText = styled.Text`
+  font-size: 25px;
+  padding: 10px;
+`;
+const BtnsContainer = styled.View`
+  padding: 40px 0;
+  align-items: center;
+`;
+const Button = styled.TouchableOpacity`
+  margin-bottom: 20px;
+  background-color: #fdbe00;
+  width: 130px;
+  padding: 10px;
+  justify-content: center;
+  flex-direction: row;
+  border-radius: 10px;
+`;
+const BtnText = styled.Text`
+  color: white;
+`;
 const FontSize = () => {
   const { fontSize, setFontSize } = useContext(Context);
   const sizes = [
-    { name: "Medium", size: 25 },
     { name: "Small", size: 15 },
+    { name: "Medium", size: 22 },
     { name: "Large", size: 30 },
   ];
   return (
     <View>
-      <Text>FontSize</Text>
-      <Text>Current Size: {fontSize.name} </Text>
-      {sizes.map((size) => {
-        return <Button title={size.name} onPress={() => setFontSize(size)} />;
-      })}
+      <StyledText>Current Size: {fontSize.name} </StyledText>
+      <BtnsContainer>
+        {sizes.map((size) => {
+          return (
+            <Button onPress={() => setFontSize(size)}>
+              <BtnText style={{ fontSize: size.size }}>{size.name}</BtnText>
+            </Button>
+          );
+        })}
+      </BtnsContainer>
     </View>
   );
 };
