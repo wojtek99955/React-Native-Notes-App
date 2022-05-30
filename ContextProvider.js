de2bf9f9d9e9128a45913a86dbb19d296f5d441d
Wojtek Książek <wojtek99955@gmail.com>
2022-05-30 14:31:42 +0200

    code refactor

--- ContextProvider.js
+++ ContextProvider.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { createContext, useState } from "react";
 
 export const Context = createContext();
@@ -6,8 +5,16 @@ export const Context = createContext();
 function ContextProvider({ children }) {
   const [notes, setNotes] = useState([]);
   const [searchNote, setSearchNote] = useState("");
+
   return (
-    <Context.Provider value={{ notes, setNotes, searchNote, setSearchNote }}>
+    <Context.Provider
+      value={{
+        notes,
+        setNotes,
+        searchNote,
+        setSearchNote,
+      }}
+    >
       {children}
     </Context.Provider>
   );
