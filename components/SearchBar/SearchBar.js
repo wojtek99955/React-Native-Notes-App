d43a1ed74f62ba541e70560a047d0f53b2d6afcc
Wojtek Książek <wojtek99955@gmail.com>
2022-05-30 14:32:48 +0200

    search note added

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -1,12 +1,16 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Wrapper, Container, Input } from "./SearchBarStyles";
+import { Context } from "../../ContextProvider";
+import { useContext } from "react";
 
 const SearchBar = () => {
+  const { setSearchNote } = useContext(Context);
+
   return (
     <Wrapper>
       <Container>
         <Ionicons name="search-outline" size={20} />
-        <Input placeholder="Search notes" />
+        <Input placeholder="Search notes" onChangeText={setSearchNote} />
       </Container>
     </Wrapper>
   );
