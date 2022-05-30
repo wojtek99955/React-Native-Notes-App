3f230629aa5e19303df000d629e04be8c15a6462
Wojtek Książek <wojtek99955@gmail.com>
2022-05-30 16:12:53 +0200

    font size btns added

--- components/Settings/FontSize.js
+++ components/Settings/FontSize.js
@@ -1,10 +1,21 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Button } from "react-native";
+import { useContext } from "react";
+import { Context } from "../../ContextProvider";
 
 const FontSize = () => {
+  const { fontSize, setFontSize } = useContext(Context);
+  const sizes = [
+    { name: "Medium", size: 25 },
+    { name: "Small", size: 15 },
+    { name: "Large", size: 30 },
+  ];
   return (
     <View>
       <Text>FontSize</Text>
+      <Text>Current Size: {fontSize.name} </Text>
+      {sizes.map((size) => {
+        return <Button title={size.name} onPress={() => setFontSize(size)} />;
+      })}
     </View>
   );
 };
