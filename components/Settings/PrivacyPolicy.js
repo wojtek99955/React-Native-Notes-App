6fff02bb61368874bbdf18a97c56e653376edced
Wojtek Książek <wojtek99955@gmail.com>
2022-05-30 15:32:38 +0200

    PrivacyPolicy added

---
+++ components/Settings/PrivacyPolicy.js
@@ -0,0 +1,19 @@
+import { View, Text } from "react-native";
+
+const PrivacyPolicy = () => {
+  return (
+    <View>
+      <Text>PrivacyPolicy</Text>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi neque
+        iste doloremque repellendus qui alias voluptatibus inventore ex quam,
+        aliquam explicabo doloribus earum impedit, voluptate dolor amet rerum
+        laudantium voluptas odio voluptates soluta, voluptatum ipsam dolorum?
+        Aperiam, doloribus? Cum soluta eos laudantium, quibusdam minus
+        temporibus assumenda quia voluptate suscipit animi.
+      </Text>
+    </View>
+  );
+};
+
+export default PrivacyPolicy;
