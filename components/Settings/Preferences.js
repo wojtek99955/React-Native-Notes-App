f88bdb89b5644bb6977e68c98bd9430a2f34ca2a
Wojtek Książek <wojtek99955@gmail.com>
2022-05-30 15:37:55 +0200

    Dark Mode added to stack

--- components/Settings/Preferences.js
+++ components/Settings/Preferences.js
@@ -1,8 +1,17 @@
 import { View, Text } from "react-native";
 import React from "react";
+import styled from "styled-components";
+const PreferencesLink = styled.TouchableOpacity``;
+const LinkText = styled.Text``;
 
-const Preferences = () => {
-  return <View></View>;
+const Preferences = ({ navigation }) => {
+  return (
+    <View>
+      <PreferencesLink onPress={() => navigation.navigate("Dark Mode")}>
+        <LinkText>Dark Mode</LinkText>
+      </PreferencesLink>
+    </View>
+  );
 };
 
 export default Preferences;
--- screens/SettingsScreens.js
+++ screens/SettingsScreens.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Settings from "../components/Settings/Settings";
 import Preferences from "../components/Settings/Preferences";
 import PrivacyPolicy from "../components/Settings/PrivacyPolicy";
+import DarkMode from "../components/Settings/DarkMode";
 
 const SettingsScreens = () => {
   const Stack = createStackNavigator();
@@ -16,6 +17,7 @@ const SettingsScreens = () => {
       />
       <Stack.Screen name="Preferences" component={Preferences} />
       <Stack.Screen name="Privacy Policy" component={PrivacyPolicy} />
+      <Stack.Screen name="Dark Mode" component={DarkMode} />
     </Stack.Navigator>
   );
 };
