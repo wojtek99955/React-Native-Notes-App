6ec43063e76b32cb8050fb11646e11384d27cce7
Wojtek Książek <wojtek99955@gmail.com>
2022-05-31 13:14:00 +0200

    delete icon added

--- components/Details.js
+++ components/Details.js
@@ -2,7 +2,11 @@ import { View, Text } from "react-native";
 import styled from "styled-components";
 import { Context } from "../ContextProvider";
 import { useContext } from "react";
-
+import Ionicons from "@expo/vector-icons/Ionicons";
+const Wrapper = styled.View`
+  flex: 1;
+  position: relative;
+`;
 const Container = styled.View`
   background-color: white;
   margin: 30px 10px;
@@ -15,14 +19,24 @@ const NoteText = styled.Text`
 const Date = styled.Text`
   margin-top: 40px;
 `;
+const IconContainer = styled.View`
+  position: absolute;
+  bottom: 100px;
+  right: 50px;
+`;
 const Details = ({ route }) => {
   const item = route.params;
   const { fontSize } = useContext(Context);
   return (
-    <Container>
-      <NoteText fontSize={fontSize.size}>{item.text}</NoteText>
-      <Date>{item.date}</Date>
-    </Container>
+    <Wrapper>
+      <Container>
+        <NoteText fontSize={fontSize.size}>{item.text}</NoteText>
+        <Date>{item.date}</Date>
+      </Container>
+      <IconContainer>
+        <Ionicons name="close-circle" size={60} color="red" />
+      </IconContainer>
+    </Wrapper>
   );
 };
 
