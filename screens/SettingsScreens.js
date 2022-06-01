d02b36222d552eaa45cda0b0dda1c0d5c42a9eae
Wojtek Książek <wojtek99955@gmail.com>
2022-06-01 13:45:51 +0200

    FontColor added to the stack

--- screens/SettingsScreens.js
+++ screens/SettingsScreens.js
@@ -7,6 +7,7 @@ import PrivacyPolicy from "../components/Settings/PrivacyPolicy";
 import DarkMode from "../components/Settings/DarkMode";
 import About from "../components/Settings/About";
 import FontSize from "../components/Settings/FontSize";
+import FontColor from "../components/Settings/FontColor";
 
 const SettingsScreens = () => {
   const Stack = createStackNavigator();
@@ -22,6 +23,7 @@ const SettingsScreens = () => {
       <Stack.Screen name="Dark Mode" component={DarkMode} />
       <Stack.Screen name="About" component={About} />
       <Stack.Screen name="Font Size" component={FontSize} />
+      <Stack.Screen name="Font Color" component={FontColor} />
     </Stack.Navigator>
   );
 };
