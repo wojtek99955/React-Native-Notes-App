90eb2336007d674dea5d114673046f1da2a994fd
Wojtek Książek <wojtek99955@gmail.com>
2022-06-01 13:52:45 +0200

    styles update / state added

--- components/Settings/FontColor.js
+++ components/Settings/FontColor.js
@@ -1,10 +1,21 @@
 import { View, Text } from "react-native";
+import { useContext } from "react";
+import { Context } from "../../ContextProvider";
+import styled from "styled-components";
+
+const Container = styled.View``;
+const MainText = styled.Text`
+  font-size: 25px;
+  margin: auto;
+  padding-top: 20px;
+`;
 
 const FontColor = () => {
+  const { fontColor, setFontColor } = useContext(Context);
   return (
-    <View>
-      <Text>FontColor</Text>
-    </View>
+    <Container>
+      <MainText>Current font color: {fontColor}</MainText>
+    </Container>
   );
 };
 
