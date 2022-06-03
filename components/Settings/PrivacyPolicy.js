ec6b7921e632dc1d14599a133096b3aed0171e8a
Wojtek Książek <wojtek99955@gmail.com>
2022-06-03 11:25:39 +0200

    styles update

--- components/Settings/PrivacyPolicy.js
+++ components/Settings/PrivacyPolicy.js
@@ -1,18 +1,46 @@
 import { View, Text } from "react-native";
+import styled from "styled-components";
 
+const Container = styled.ScrollView`
+  padding: 15px;
+`;
+const StyledText = styled.Text`
+  font-size: 18px;
+  margin: 50px 0;
+`;
+const Title = styled.Text`
+  font-size: 25px;
+  text-align: center;
+`;
 const PrivacyPolicy = () => {
   return (
-    <View>
-      <Text>PrivacyPolicy</Text>
-      <Text>
+    <Container>
+      <Title>PrivacyPolicy</Title>
+      <StyledText>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi neque
         iste doloremque repellendus qui alias voluptatibus inventore ex quam,
         aliquam explicabo doloribus earum impedit, voluptate dolor amet rerum
         laudantium voluptas odio voluptates soluta, voluptatum ipsam dolorum?
         Aperiam, doloribus? Cum soluta eos laudantium, quibusdam minus
         temporibus assumenda quia voluptate suscipit animi.
-      </Text>
-    </View>
+      </StyledText>
+      <StyledText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi neque
+        iste doloremque repellendus qui alias voluptatibus inventore ex quam,
+        aliquam explicabo doloribus earum impedit, voluptate dolor amet rerum
+        laudantium voluptas odio voluptates soluta, voluptatum ipsam dolorum?
+        Aperiam, doloribus? Cum soluta eos laudantium, quibusdam minus
+        temporibus assumenda quia voluptate suscipit animi.
+      </StyledText>
+      <StyledText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi neque
+        iste doloremque repellendus qui alias voluptatibus inventore ex quam,
+        aliquam explicabo doloribus earum impedit, voluptate dolor amet rerum
+        laudantium voluptas odio voluptates soluta, voluptatum ipsam dolorum?
+        Aperiam, doloribus? Cum soluta eos laudantium, quibusdam minus
+        temporibus assumenda quia voluptate suscipit animi.
+      </StyledText>
+    </Container>
   );
 };
 
