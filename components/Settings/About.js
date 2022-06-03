8c4f8e4c254243421a386b49f11bca689657edee
Wojtek Książek <wojtek99955@gmail.com>
2022-06-03 13:04:24 +0200

    styles update

--- components/Settings/About.js
+++ components/Settings/About.js
@@ -1,15 +1,24 @@
 import { View, Text } from "react-native";
+import styled from "styled-components";
+
+const Container = styled.View`
+  padding: 10px;
+`;
+const StyledText = styled.Text`
+  font-size: 18px;
+  margin-top: 20px;
+`;
 
 const About = () => {
   return (
-    <View>
-      <Text>
+    <Container>
+      <StyledText>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dicta
         nostrum voluptates cupiditate tempora facilis assumenda quam nihil,
         delectus placeat! Numquam quibusdam culpa quas ipsum, veritatis ut
         quisquam assumenda maiores!
-      </Text>
-    </View>
+      </StyledText>
+    </Container>
   );
 };
 
