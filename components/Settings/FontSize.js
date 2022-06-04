724fcb7dc28392d475b4ad3a2882b0b45d6c5d31
Wojtek Książek <wojtek99955@gmail.com>
2022-06-04 12:14:38 +0200

    FontSize code refactor

--- components/Settings/FontSize.js
+++ components/Settings/FontSize.js
@@ -1,31 +1,14 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View } from "react-native";
 import { useContext } from "react";
 import { Context } from "../../ContextProvider";
-import styled from "styled-components";
+import {
+  StyledText,
+  BtnsContainer,
+  BtnText,
+  Button,
+  DemoText,
+} from "./FontSize/FontSizeStyles";
 
-const StyledText = styled.Text`
-  font-size: 25px;
-  padding: 10px;
-`;
-const BtnsContainer = styled.View`
-  padding: 40px 0;
-  align-items: center;
-`;
-const Button = styled.TouchableOpacity`
-  margin-bottom: 20px;
-  background-color: #fdbe00;
-  width: 130px;
-  padding: 10px;
-  justify-content: center;
-  flex-direction: row;
-  border-radius: 10px;
-`;
-const BtnText = styled.Text`
-  color: white;
-`;
-const DemoText = styled.Text`
-  font-size: ${({ fontSize }) => fontSize.size}px;
-`;
 const FontSize = () => {
   const { fontSize, setFontSize } = useContext(Context);
   const sizes = [
---
+++ components/Settings/FontSize/FontSizeStyles.js
@@ -0,0 +1,26 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import styled from "styled-components";
+
+export const StyledText = styled.Text`
+  font-size: 25px;
+  padding: 10px;
+`;
+export const BtnsContainer = styled.View`
+  padding: 40px 0;
+  align-items: center;
+`;
+export const Button = styled.TouchableOpacity`
+  margin-bottom: 20px;
+  background-color: #fdbe00;
+  width: 130px;
+  padding: 10px;
+  justify-content: center;
+  flex-direction: row;
+  border-radius: 10px;
+`;
+export const BtnText = styled.Text`
+  color: white;
+`;
+export const DemoText = styled.Text`
+  font-size: ${({ fontSize }) => fontSize.size}px;
+`;
