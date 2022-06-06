7edcfc611e945163acaf2846c2c8daf8e568002e
Wojtek Książek <wojtek99955@gmail.com>
2022-06-06 15:54:07 +0200

    edit handler added

--- components/Details/Details.js
+++ components/Details/Details.js
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { useState } from "react";
 import DeleteModal from "./DeleteModal";
+import { TextInput, Pressable } from "react-native";
 import {
   Wrapper,
   Container,
@@ -17,18 +18,38 @@ const Details = ({ route, navigation }) => {
   const { fontSize } = useContext(Context);
   const [modalVisible, setModalVisible] = useState(false);
   const openModal = () => setModalVisible(true);
+  const { notes, setNotes } = useContext(Context);
+  const [edit, setEdit] = useState(false);
+  const openEdit = () => {
+    setEdit(true);
+  };
 
   return (
     <Wrapper>
       <Container>
-        <NoteText fontSize={fontSize.size}>{item.text}</NoteText>
-        <Date>{item.date}</Date>
+        {edit ? (
+          <>
+            <TextInput defaultValue={item.text} />
+            <Date>{item.date}</Date>
+          </>
+        ) : (
+          <>
+            <NoteText fontSize={fontSize.size}>{item.text}</NoteText>
+            <Date>{item.date}</Date>
+          </>
+        )}
       </Container>
+      <Date>{edit + "cos"}</Date>
+      <Date>{item.openEdditing.toString()}</Date>
       <BtnsWrapper>
         <BtnContainer onPress={openModal}>
           <Ionicons name="close-circle" size={50} color="red" />
         </BtnContainer>
-        <BtnContainer>
+        <BtnContainer
+          onPress={() => {
+            openEdit();
+          }}
+        >
           <Ionicons name="create-outline" size={50} color="grey" />
         </BtnContainer>
       </BtnsWrapper>
