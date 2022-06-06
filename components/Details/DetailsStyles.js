acd9f3518f8ce26be78e2f7d1a6635538999a53d
Wojtek Książek <wojtek99955@gmail.com>
2022-06-06 17:55:50 +0200

    styles update

--- components/Details/DetailsStyles.js
+++ components/Details/DetailsStyles.js
@@ -36,7 +36,6 @@ export const BtnsWrapper = styled.View`
 
 export const StyledInput = styled.TextInput`
   font-size: ${({ fontSize }) => `${fontSize}px`};
-  flex: 1;
 `;
 
 export const SaveBtn = styled.TouchableOpacity`
