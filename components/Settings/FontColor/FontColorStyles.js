f48d806f3c46209b2e88b78138f5e61d6d35f357
Wojtek Książek <wojtek99955@gmail.com>
2022-06-07 12:11:44 +0200

    border radius bug fixed

--- components/Settings/FontColor/FontColorStyles.js
+++ components/Settings/FontColor/FontColorStyles.js
@@ -16,7 +16,7 @@ export const ColorContainer = styled.View`
 export const Color = styled.View`
   width: 50px;
   height: 50px;
-  border-radius: 50%;
+  border-radius: 25px;
   background-color: ${({ color }) => color};
 `;
 
