af1bd714f2bd7f992ae17acdf5db408fb4020dfc
Wojtek Książek <wojtek99955@gmail.com>
2022-06-08 18:17:14 +0200

    dark styles applied

--- App.js
+++ App.js
@@ -7,79 +7,84 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import NoteListScreen from "./screens/NoteListScreen";
 import "react-native-gesture-handler";
 import SettingsScreens from "./screens/SettingsScreens";
-import { useContext } from "react";
+import { useContext, createContext } from "react";
 import { Context } from "./ContextProvider";
 import { Appearance } from "react-native";
 const theme = Appearance.getColorScheme();
+
+export const ThemeContext = createContext();
 
 export default function App() {
   const ctx = useContext(Context);
   const Tabs = createBottomTabNavigator();
   return (
     <ContextProvider>
-      <NavigationContainer>
-        <Tabs.Navigator
-          screenOptions={{
-            tabBarActiveTintColor: "#fdbe00",
-            tabBarInactiveTintColor: "gray",
-            tabBarHideOnKeyboard: true,
-            headerStyle: {
-              backgroundColor: theme === "dark" ? "black" : "white",
-            },
-            headerTintColor: theme === "dark" ? "white" : "black",
-            tabBarLabelStyle: {
-              fontSize: 15,
-            },
-            tabBarStyle: {
-              paddingVertical: 5,
-              height: Platform.OS === "ios" ? 85 : 55,
-              backgroundColor: theme === "dark" ? "black" : "white",
-            },
-          }}
-        >
-          <Tabs.Screen
-            name="Create Note"
-            component={AddNote}
-            options={{
-              tabBarIcon: ({ focused }) => (
-                <Ionicons
-                  name={focused ? "add-circle" : "add-circle-outline"}
-                  color={focused ? "#fdbe00" : "gray"}
-                  size={28}
-                />
-              ),
+      <ThemeContext.Provider value={theme}>
+        <NavigationContainer>
+          <Tabs.Navigator
+            screenOptions={{
+              tabBarActiveTintColor: "#fdbe00",
+              tabBarInactiveTintColor: "gray",
+              tabBarHideOnKeyboard: true,
+              headerStyle: {
+                backgroundColor: theme === "dark" ? "#333333" : "white",
+              },
+              headerTintColor: theme === "dark" ? "white" : "black",
+
+              tabBarLabelStyle: {
+                fontSize: 15,
+              },
+              tabBarStyle: {
+                paddingVertical: 5,
+                height: Platform.OS === "ios" ? 85 : 55,
+                backgroundColor: theme === "dark" ? "#242424" : "white",
+              },
             }}
-          />
-          <Tabs.Screen
-            name="Notes List"
-            component={NoteListScreen}
-            options={{
-              tabBarIcon: ({ focused }) => (
-                <Ionicons
-                  name={focused ? "document" : "document-outline"}
-                  color={focused ? "#fdbe00" : "gray"}
-                  size={28}
-                />
-              ),
-              headerShown: false,
-            }}
-          />
-          <Tabs.Screen
-            name="Settings"
-            component={SettingsScreens}
-            options={{
-              tabBarIcon: ({ focused }) => (
-                <Ionicons
-                  name={focused ? "settings" : "settings-outline"}
-                  color={focused ? "#fdbe00" : "gray"}
-                  size={28}
-                />
-              ),
-              headerShown: false,
-            }}
-          />
-        </Tabs.Navigator>
-      </NavigationContainer>
+          >
+            <Tabs.Screen
+              name="Create Note"
+              component={AddNote}
+              options={{
+                tabBarIcon: ({ focused }) => (
+                  <Ionicons
+                    name={focused ? "add-circle" : "add-circle-outline"}
+                    color={focused ? "#fdbe00" : "gray"}
+                    size={28}
+                  />
+                ),
+              }}
+            />
+            <Tabs.Screen
+              name="Notes List"
+              component={NoteListScreen}
+              options={{
+                tabBarIcon: ({ focused }) => (
+                  <Ionicons
+                    name={focused ? "document" : "document-outline"}
+                    color={focused ? "#fdbe00" : "gray"}
+                    size={28}
+                  />
+                ),
+                headerShown: false,
+              }}
+            />
+            <Tabs.Screen
+              name="Settings"
+              component={SettingsScreens}
+              options={{
+                tabBarIcon: ({ focused }) => (
+                  <Ionicons
+                    name={focused ? "settings" : "settings-outline"}
+                    color={focused ? "#fdbe00" : "gray"}
+                    size={28}
+                  />
+                ),
+                headerShown: false,
+              }}
+            />
+          </Tabs.Navigator>
+        </NavigationContainer>
+      </ThemeContext.Provider>
     </ContextProvider>
   );
 }
