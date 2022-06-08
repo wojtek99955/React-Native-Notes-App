512d616d38be0963b784f7e8b3c7ecc845a8dab7
Wojtek Książek <wojtek99955@gmail.com>
2022-06-08 19:12:09 +0200

    dark styles applied

--- components/Settings/Settings.js
+++ components/Settings/Settings.js
@@ -1,10 +1,13 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import styled from "styled-components";
+import { Context } from "../../ContextProvider";
+import { useContext } from "react";
 const PreferencesLink = styled.TouchableOpacity`
   padding: 10px;
 `;
 const LinkText = styled.Text`
   font-size: 25px;
+  color: ${({ theme }) => (theme === "dark" ? "white" : "black")};
 `;
 const Divider = styled.View`
   width: 100%;
@@ -12,17 +15,20 @@ const Divider = styled.View`
   border-bottom-width: 1px;
 `;
 const Container = styled.View`
-  padding: 10px;
+  padding: 0 10px;
+  background-color: ${({ theme }) => (theme === "dark" ? "black" : "white")};
+  flex: 1;
 `;
 const Settings = ({ navigation }) => {
   const links = ["Preferences", "Privacy Policy", "About"];
+  const { theme } = useContext(Context);
   return (
-    <Container>
+    <Container theme={theme}>
       {links.map((link) => {
         return (
           <View key={link}>
             <PreferencesLink onPress={() => navigation.navigate(link)}>
-              <LinkText>{link}</LinkText>
+              <LinkText theme={theme}>{link}</LinkText>
             </PreferencesLink>
             <Divider></Divider>
           </View>
