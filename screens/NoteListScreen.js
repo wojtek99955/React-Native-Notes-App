5fd1f8b40c8e9a8bacc20a9fed062c279f05093e
Wojtek Książek <wojtek99955@gmail.com>
2022-06-08 18:13:25 +0200

    dark styles applied

--- screens/NoteListScreen.js
+++ screens/NoteListScreen.js
@@ -2,11 +2,21 @@ import { View, Text } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import NotesList from "../components/NotesList/NotesList";
 import Details from "../components/Details/Details/Details";
+import { Context } from "../ContextProvider";
+import { useContext } from "react";
 
 const Stack = createStackNavigator();
 const NoteListScreen = () => {
+  const { theme } = useContext(Context);
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme === "dark" ? "#242424" : "white",
+        },
+        headerTintColor: theme === "dark" ? "white" : "black",
+      }}
+    >
       <Stack.Screen
         name="List"
         options={{ title: "Notes List" }}
