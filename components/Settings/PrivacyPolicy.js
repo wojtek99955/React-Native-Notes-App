589d66cd5968bc84ed0328a12d205f407d854651
Wojtek Książek <wojtek99955@gmail.com>
2022-06-09 11:00:25 +0200

    dark styles applied

--- components/Settings/PrivacyPolicy.js
+++ components/Settings/PrivacyPolicy.js
@@ -1,22 +1,29 @@
 import { View, Text } from "react-native";
 import styled from "styled-components";
+import { useContext } from "react";
+import { Context } from "../../ContextProvider";
 
 const Container = styled.ScrollView`
   padding: 15px;
+  flex: 1;
+  background-color: ${({ theme }) => (theme === "dark" ? "black" : "white")};
 `;
 const StyledText = styled.Text`
   font-size: 18px;
   margin: 50px 0;
+  color: ${({ theme }) => (theme === "dark" ? "white" : "black")};
 `;
 const Title = styled.Text`
   font-size: 25px;
   text-align: center;
+  color: ${({ theme }) => (theme === "dark" ? "white" : "black")};
 `;
 const PrivacyPolicy = () => {
+  const { theme } = useContext(Context);
   return (
-    <Container>
-      <Title>PrivacyPolicy</Title>
-      <StyledText>
+    <Container theme={theme}>
+      <Title theme={theme}>PrivacyPolicy</Title>
+      <StyledText theme={theme}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi neque
         iste doloremque repellendus qui alias voluptatibus inventore ex quam,
         aliquam explicabo doloribus earum impedit, voluptate dolor amet rerum
@@ -24,7 +31,7 @@ const PrivacyPolicy = () => {
         Aperiam, doloribus? Cum soluta eos laudantium, quibusdam minus
         temporibus assumenda quia voluptate suscipit animi.
       </StyledText>
-      <StyledText>
+      <StyledText theme={theme}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi neque
         iste doloremque repellendus qui alias voluptatibus inventore ex quam,
         aliquam explicabo doloribus earum impedit, voluptate dolor amet rerum
@@ -32,7 +39,7 @@ const PrivacyPolicy = () => {
         Aperiam, doloribus? Cum soluta eos laudantium, quibusdam minus
         temporibus assumenda quia voluptate suscipit animi.
       </StyledText>
-      <StyledText>
+      <StyledText theme={theme}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi neque
         iste doloremque repellendus qui alias voluptatibus inventore ex quam,
         aliquam explicabo doloribus earum impedit, voluptate dolor amet rerum
