a4d07eb5301c87c8a9537a647b6e3fbad7969180
Wojtek Książek <wojtek99955@gmail.com>
2022-06-09 11:03:10 +0200

    dark styles applied

--- components/Settings/About.js
+++ components/Settings/About.js
@@ -1,18 +1,25 @@
 import { View, Text } from "react-native";
 import styled from "styled-components";
+import { useContext } from "react";
+import { Context } from "../../ContextProvider";
 
 const Container = styled.View`
   padding: 10px;
+  background-color: ${({ theme }) => (theme === "dark" ? "black" : "white")};
+  flex: 1;
 `;
 const StyledText = styled.Text`
   font-size: 18px;
   margin-top: 20px;
+  color: ${({ theme }) => (theme === "dark" ? "white" : "black")};
 `;
 
 const About = () => {
+  const { theme } = useContext(Context);
+
   return (
-    <Container>
-      <StyledText>
+    <Container theme={theme}>
+      <StyledText theme={theme}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dicta
         nostrum voluptates cupiditate tempora facilis assumenda quam nihil,
         delectus placeat! Numquam quibusdam culpa quas ipsum, veritatis ut
