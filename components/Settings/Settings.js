d0f720ed1fe0b2621ddbf9492da9a014cf0c2aab
Wojtek Książek <wojtek99955@gmail.com>
2022-06-09 11:27:25 +0200

    redundant code removed

--- components/Settings/Settings.js
+++ components/Settings/Settings.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View } from "react-native";
 import styled from "styled-components";
 import { Context } from "../../ContextProvider";
 import { useContext } from "react";
