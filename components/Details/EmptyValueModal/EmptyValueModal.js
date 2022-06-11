4a3a1615fbc59a087acc8be83acd7b72ad6a3185
Wojtek Książek <wojtek99955@gmail.com>
2022-06-11 12:11:49 +0200

    styles update

--- components/Details/EmptyValueModal/EmptyValueModal.js
+++ components/Details/EmptyValueModal/EmptyValueModal.js
@@ -1,4 +1,4 @@
-import { View, Text, Modal, Button } from "react-native";
+import { View, Text, Modal, TouchableOpacity } from "react-native";
 import React from "react";
 import styled from "styled-components";
 export const ModalContainer = styled.View`
@@ -7,17 +7,40 @@ export const ModalContainer = styled.View`
   flex: 1;
   background-color: rgba(0, 0, 0, 0.8);
 `;
+export const Button = styled.TouchableOpacity`
+  background-color: #fdbe00;
+  margin: 15px;
+  border-radius: 10px;
+  width: 110px;
+  align-items: center;
+  justify-content: center;
+  height: 45px;
+`;
+
+export const ButtonText = styled.Text`
+  color: white;
+  font-size: 20px;
+  font-weight: 700;
+`;
+export const ModalText = styled.Text`
+  font-size: 40px;
+  text-align: center;
+  color: white;
+  margin-bottom: 60px;
+`;
 
 const EmptyValueModal = ({ modalVisible, setModalVisible }) => {
   return (
     <Modal transparent={true} visible={modalVisible}>
       <ModalContainer>
+        <ModalText>You can't save empty note</ModalText>
         <Button
-          title="ok"
           onPress={() => {
             setModalVisible(false);
           }}
-        />
+        >
+          <ButtonText>ok</ButtonText>
+        </Button>
       </ModalContainer>
     </Modal>
   );
